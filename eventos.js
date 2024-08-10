c34d1ce723ea634c791b33625e89e372b0915525
aguslelop <aguslemeslopez@gmail.com>
2024-08-09 22:21:20 -0300

    miDiv

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Selecciona el div por su id
+var miDiv = document.getElementById('miDiv');
+
+// Agrega el evento 'click' al div
+miDiv.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
